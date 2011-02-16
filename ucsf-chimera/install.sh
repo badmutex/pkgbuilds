ff40f01aac2bfc42acc70554aca9744978c0cc14
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2011-02-16 17:39:17 -0500

    new package: ucsf-chimera

---
+++ ucsf-chimera/install.sh
@@ -0,0 +1,6 @@
+## arg 1:  the new package version
+pre_install() {
+	echo "============================================================================================================================================="
+	echo "By installing UCSF Chimera you agree to the terms given in the provided LICENSE file as shown on http://www.cgl.ucsf.edu/chimera/license.html"
+	echo "============================================================================================================================================="
+}
