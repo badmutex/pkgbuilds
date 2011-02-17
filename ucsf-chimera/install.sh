b08040b6a20e197903e074936a9a86b2203c20f9
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2011-02-17 13:39:34 -0500

    ucsf-chimera: added .desktop file for menu access

--- ucsf-chimera/install.sh
+++ ucsf-chimera/install.sh
@@ -4,3 +4,10 @@ pre_install() {
 	echo "By installing UCSF Chimera you agree to the terms given in the provided LICENSE file as shown on http://www.cgl.ucsf.edu/chimera/license.html"
 	echo "============================================================================================================================================="
 }
+
+
+## arg 1:  the new package version
+post_install() {
+	update-desktop-database -q
+	update-mime-database usr/share/mime &> /dev/null
+}
